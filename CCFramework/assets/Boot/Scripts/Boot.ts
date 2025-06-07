e0826e6504741951db8e57b71ce6c1014f9321e8
zhaowenlong <zhao@an-idear.com>
2025-06-07 15:44:29 +0800

    初始化

---
+++ CCFramework/assets/Boot/Scripts/Boot.ts
@@ -0,0 +1,14 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('Boot')
+export class Boot extends Component {
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+        
+    }
+}
+
