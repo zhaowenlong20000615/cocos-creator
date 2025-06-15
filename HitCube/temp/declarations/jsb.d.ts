279ae43cdb51b37c48f586c4f2d276fd3d251403
zhaowenlong20000615 <ZHAOWENLONG0615@163.com>
2025-06-15 21:03:21 +0800

    合并

--- HitCube/temp/declarations/jsb.d.ts
+++ HitCube/temp/declarations/jsb.d.ts
@@ -1,1 +1,1 @@
-/// <reference path="/Applications/Cocos/Creator/3.8.6/CocosCreator.app/Contents/Resources/resources/3d/engine/@types/jsb.d.ts"/>
+/// <reference path="D:\cocos-edit\Creator\3.8.6\resources\resources\3d\engine\@types\jsb.d.ts"/>
