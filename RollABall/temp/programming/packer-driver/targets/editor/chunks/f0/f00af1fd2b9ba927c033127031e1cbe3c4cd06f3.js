c6d4d3f3e462340049f79a7d3f3bc918574bb9cf
zhaowenlong <zhao@an-idear.com>
2025-06-19 11:13:57 +0800

    吃食物40%

---
+++ RollABall/temp/programming/packer-driver/targets/editor/chunks/f0/f00af1fd2b9ba927c033127031e1cbe3c4cd06f3.js
@@ -0,0 +1,39 @@
+System.register(["cc"], function (_export, _context) {
+  "use strict";
+
+  var _cclegacy, __checkObsolete__, __checkObsoleteInNamespace__, _decorator, Component, _dec, _class, _crd, ccclass, property, Fo_o_d;
+
+  return {
+    setters: [function (_cc) {
+      _cclegacy = _cc.cclegacy;
+      __checkObsolete__ = _cc.__checkObsolete__;
+      __checkObsoleteInNamespace__ = _cc.__checkObsoleteInNamespace__;
+      _decorator = _cc._decorator;
+      Component = _cc.Component;
+    }],
+    execute: function () {
+      _crd = true;
+
+      _cclegacy._RF.push({}, "95bb9Bi87hLJIaJ2BYyxNG8", "Fo'o'd", undefined);
+
+      __checkObsolete__(['_decorator', 'Component', 'Node']);
+
+      ({
+        ccclass,
+        property
+      } = _decorator);
+
+      _export("Fo_o_d", Fo_o_d = (_dec = ccclass('Fo_o_d'), _dec(_class = class Fo_o_d extends Component {
+        start() {}
+
+        update(deltaTime) {}
+
+      }) || _class));
+
+      _cclegacy._RF.pop();
+
+      _crd = false;
+    }
+  };
+});
+//# sourceMappingURL=f00af1fd2b9ba927c033127031e1cbe3c4cd06f3.js.map
---
+++ RollABall/temp/programming/packer-driver/targets/preview/chunks/4b/4b3173819b2d77b4ef6dac0314c5fba0edce6072.js
@@ -0,0 +1,62 @@
+System.register(["cc"], function (_export, _context) {
+  "use strict";
+
+  var _cclegacy, __checkObsolete__, __checkObsoleteInNamespace__, _decorator, Component, Vec3, _dec, _class, _class2, _descriptor, _crd, ccclass, property, Food;
+
+  function _initializerDefineProperty(target, property, descriptor, context) { if (!descriptor) return; Object.defineProperty(target, property, { enumerable: descriptor.enumerable, configurable: descriptor.configurable, writable: descriptor.writable, value: descriptor.initializer ? descriptor.initializer.call(context) : void 0 }); }
+
+  function _applyDecoratedDescriptor(target, property, decorators, descriptor, context) { var desc = {}; Object.keys(descriptor).forEach(function (key) { desc[key] = descriptor[key]; }); desc.enumerable = !!desc.enumerable; desc.configurable = !!desc.configurable; if ('value' in desc || desc.initializer) { desc.writable = true; } desc = decorators.slice().reverse().reduce(function (desc, decorator) { return decorator(target, property, desc) || desc; }, desc); if (context && desc.initializer !== void 0) { desc.value = desc.initializer ? desc.initializer.call(context) : void 0; desc.initializer = undefined; } if (desc.initializer === void 0) { Object.defineProperty(target, property, desc); desc = null; } return desc; }
+
+  function _initializerWarningHelper(descriptor, context) { throw new Error('Decorating class property failed. Please ensure that ' + 'transform-class-properties is enabled and runs after the decorators transform.'); }
+
+  return {
+    setters: [function (_cc) {
+      _cclegacy = _cc.cclegacy;
+      __checkObsolete__ = _cc.__checkObsolete__;
+      __checkObsoleteInNamespace__ = _cc.__checkObsoleteInNamespace__;
+      _decorator = _cc._decorator;
+      Component = _cc.Component;
+      Vec3 = _cc.Vec3;
+    }],
+    execute: function () {
+      _crd = true;
+
+      _cclegacy._RF.push({}, "95bb9Bi87hLJIaJ2BYyxNG8", "Food", undefined);
+
+      __checkObsolete__(['_decorator', 'Component', 'Node', 'Vec3']);
+
+      ({
+        ccclass,
+        property
+      } = _decorator);
+
+      _export("Food", Food = (_dec = ccclass('Food'), _dec(_class = (_class2 = class Food extends Component {
+        constructor() {
+          super(...arguments);
+
+          _initializerDefineProperty(this, "rotateSpeed", _descriptor, this);
+        }
+
+        start() {}
+
+        update(deltaTime) {
+          var eulerAngles = this.node.eulerAngles;
+          this.node.eulerAngles = new Vec3(eulerAngles.x, eulerAngles.y + this.rotateSpeed * deltaTime, eulerAngles.z);
+        }
+
+      }, (_descriptor = _applyDecoratedDescriptor(_class2.prototype, "rotateSpeed", [property], {
+        configurable: true,
+        enumerable: true,
+        writable: true,
+        initializer: function initializer() {
+          return 90;
+        }
+      })), _class2)) || _class));
+
+      _cclegacy._RF.pop();
+
+      _crd = false;
+    }
+  };
+});
+//# sourceMappingURL=4b3173819b2d77b4ef6dac0314c5fba0edce6072.js.map
