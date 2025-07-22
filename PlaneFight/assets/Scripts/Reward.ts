862405b876c897bc35d1846d028d81cd06ac6081
zhaowenlong <zhao@an-idear.com>
2025-07-22 11:29:49 +0800

    子弹完成，总进度30%

---
+++ PlaneFight/assets/Scripts/Reward.ts
@@ -0,0 +1,28 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('Reward')
+export class Reward extends Component {
+
+    @property
+    private speed: number = 500;
+
+    @property
+    private minX: number = -200;
+
+    @property
+    private maxX: number = 200;
+
+    @property
+    private y: number = 35;
+
+
+
+    update(deltaTime: number) {
+        this.node.setPosition(this.node.position.x, this.node.position.y - this.speed * deltaTime, this.node.position.z);
+
+        if (this.node.position.y < -600) {
+          this.node.destroy();
+        }
+    }
+}
